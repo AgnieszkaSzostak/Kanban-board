5c83c13ef45a112c5f4089f99031ae493e53493a
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-05 13:30:31 +0100

    Change Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,42 +18,48 @@ function Form (){
     }
     const selectOptions = colState.map(col => <option key={col.id} value={col.id} disabled={isColumnFull(col.limit, col.id)}>{col.name}</option>)
     const formRef = React.createRef();
-    const [form, setForm] = useState(null)
+    const [form, setForm] = useState(null);
+    const [errors, setErrors] = useState(null);
     useEffect(() => {
-        console.log(form);
+        if(errors === null){
+            console.log(formRef.current.elements);
+            const {taskName, projectStage, user} = formRef.current.elements
+            setForm({
+                name: taskName.value,
+                user: user.value,
+                idColumn: Number(projectStage.value)
+            })
+        }
+    },[errors])
+    useEffect(() => {
         if(form !== null){
-            if(form.errors === null){
-                const {name, idColumn, user} = form.task;
-                taskDispatch({
-                    type: 'ADD_TASK',
-                    payload: {
-                        id: uuidv4(),
-                        name,
-                        user,
-                        idColumn: Number(idColumn),
-                    }
-                })
-                formRef.current.reset();
-            }
+            taskDispatch({
+                type: 'ADD_TASK',
+                payload: {
+                    ...form,
+                    id: uuidv4(),
+                }
+            });
+            formRef.current.reset();
         }
     },[form])
 
     return (
-        <form className="form" ref={formRef}  onSubmit={(e) =>setForm(validateForm(e, formRef.current))}>
+        <form className="form" ref={formRef}  onSubmit={(e) =>setErrors(validateForm(e, formRef.current))}>
             <label className="form__label" htmlFor="taskName"> Task name
                 <input className="form__input" type="text" name="taskName"/>
-                {form !==null && form.errors !== null && form.errors.taskName ? <span className="form__error" >{form.errors.taskName}</span> : null}
+                {errors !==null && errors.taskName ? <span className="form__error" >{errors.taskName}</span> : null}
             </label>
             <label className="form__label" htmlFor="projectStage"> Project stage
                 <select className="form__select" name="projectStage" type="select">
                     <option value=''>Select stage</option>
                     {selectOptions}
                 </select>
-                {form !==null && form.errors !== null && form.errors.projectStage ? <span className="form__error">{form.errors.projectStage}</span> : null}
+                {errors !==null && errors.projectStage ? <span className="form__error">{errors.projectStage}</span> : null}
             </label>
             <label className="form__label" htmlFor="user"> User 
                 <input className="form__input" type="text" name="user"/>
-                {form !==null && form.errors !== null && form.errors.user ? <span className="form__error">{form.errors.user}</span> : null}
+                {errors !==null  && errors.user ? <span className="form__error">{errors.user}</span> : null}
             </label>
             <input className="form__input--submit" type="submit"/>
         </form>
