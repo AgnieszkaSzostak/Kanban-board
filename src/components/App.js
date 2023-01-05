7102f40724f0dd969af729745faee4980a0be1b6
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-05 12:48:08 +0100

    Import initial columns and tasks

--- src/components/App.js
+++ src/components/App.js
@@ -7,28 +7,12 @@ import '../style.css';
 import { ColumnsContext, TasksContext } from './context';
 import Form from './Form';
 import useStorage from './hook';
+import { initialColumns, initialTasks } from './data';
 
 function App () {
-    const initialColumns = [
-        {id:1, name: 'Pending', limit: 5},
-        {id:2, name: 'Analysis - doing', limit: 3},
-        {id:3, name: 'Analysis - done', limit: 2},
-        {id:4, name: 'Development - doing', limit: 3},
-        {id:5, name: 'Development - done', limit: 2},
-        {id:6, name: 'Test', limit: 2},
-        {id:7, name: 'Deploy', limit: 5},
-    ];
-
+    
     const {Provider: ColumnProvider} = ColumnsContext;
     const {Provider: TasksProvider} = TasksContext;
-
-    const initialTasks = [
-        {id:1, name:'Task1', idColumn: 1, user: 'Ana'},
-        {id:2, name:'Task2', idColumn: 1, user: 'Ana'},
-        {id:3, name:'Task3', idColumn: 2, user: 'Ana'},
-        {id:4, name:'Task4', idColumn: 3, user: 'Ana'},
-        {id:5, name:'Task5', idColumn: 3, user: 'Ana'},
-    ]
 
     const reducer = (state, action) => {
         const newState = [...state]
@@ -62,6 +46,7 @@ function App () {
     useEffect(()=> {
         setTasks(taskState);
     },[taskState])
+    
     return (
         <ColumnProvider  value={{colState, colDispatch}}>
             <TasksProvider value={{taskState, taskDispatch}}>
